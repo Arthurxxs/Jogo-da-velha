b4b46ddc4fe957d1a5cd815a583532d0ebb95076
Arthur Martins Altino <Arthur53556306@edu.df.senac.br>
2025-08-26 11:39:25 -0300

    Salvo

--- OJogo/OJogo/Program.cs
+++ OJogo/OJogo/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        int posicaoLinha, posicaoColuna;
+
+        Console.WriteLine("   | |  ");
+        Console.WriteLine(" --+-+--- ");
+        Console.WriteLine("   | |  ");
+        Console.WriteLine(" --+-+--- ");
+        Console.WriteLine("   | |  ");
+
+        Console.WriteLine("Jogador, escolha a linha: ");
+        posicaoLinha = int.Parse(Console.ReadLine()) - 1
+
+        Console.WriteLine("Jogador, escolha a coluna: ");
+        posicaoColuna = int.Parse(Console.ReadLine()) - 1;
+    }
+}
