0fc4d495c1901974f4dfdf41e5cfcbcb08567255
Arthur53556306 <Arthur53556306@edu.df.senac.br>
2025-09-04 11:38:47 -0300

    04/09

--- OJogo/OJogo/Program.cs
+++ OJogo/OJogo/Program.cs
@@ -1,15 +1,14 @@
 ﻿using System.ComponentModel.Design;
 using System.Runtime.CompilerServices;
-
 public class Program
-
 {
+
     public static void Main(string[] args)
     {
         string[,] tabuleiro = new string[3, 3];
-        int posicaoLinha, posicaoColuna, qtddJogadas = 1;
+        int posicaoLinha = 0, posicaoColuna = 0, qttJogadas = 1;
         char jogadorDaVez = 'X';
-        bool deuCerto;
+        bool deuCerto = false;
         bool houveGanhador = false;
         int jogarNovamente = 0;
         int placarX = 0, placarO = 0;
@@ -19,83 +18,27 @@ public class Program
         Console.WriteLine("Com quantos jogadores você quer jogar? (1 ou 2)");
         jogadores = int.Parse(Console.ReadLine());
 
+
         if (jogadores == 2)
         {
             Console.WriteLine("Jogo para 2 jogadores selecionado.");
             do
             {
-                
+                DefinirSimbolo(jogadorDaVez);
+
                 LimparTabuleiro(tabuleiro);
 
                 MostrarTabuleiroVazio();
 
-                DefinirSimbolo(jogadorDaVez);
+
                 do
                 {
-                    do
-                    {
-                        Console.WriteLine("Jogador {0}, escolha a linha: ", jogadorDaVez);
-                        deuCerto = int.TryParse(Console.ReadLine(), out posicaoLinha);
-                        if (deuCerto == true)
-                        {
-                            posicaoLinha--;
-                        }
-                        else
-                        {
-                            Console.WriteLine("Escreva um número! Tente novamente.");
-                        }
-                    } while (deuCerto == false);
+                    
+                   
+                    
+                        EscolherPosicao(ref jogadorDaVez, ref deuCerto, ref posicaoLinha, ref posicaoColuna, ref tabuleiro, ref qttJogadas);
 
-                    do
-                    {
-                        Console.WriteLine("Jogador, escolha a coluna: ");
-                        deuCerto = int.TryParse(Console.ReadLine(), out posicaoColuna);
-                        if (deuCerto == true)
-                        {
-                            posicaoColuna--;
-                        }
-                        else
-                        {
-                            Console.WriteLine("Escreva um número! Tente novamente.");
-                        }
-                    } while (deuCerto == false);
-
-                    if (posicaoLinha >= 3 || posicaoColuna >= 3 || posicaoLinha < 0 || posicaoColuna < 0)
-                    {
-
-                        Console.WriteLine("Posição inválida! Tente novamente.");
-
-                    }
-                    else
-                    {
-                        if (tabuleiro[posicaoLinha, posicaoColuna] == " ")
-                        {
-
-                            tabuleiro[posicaoLinha, posicaoColuna] = jogadorDaVez.ToString();
-                            if (jogadorDaVez == 'X')
-                            {
-                                jogadorDaVez = 'O';
-                            }
-                            else
-                            {
-                                jogadorDaVez = 'X';
-                            }
-
-                            qtddJogadas++;
-
-                            Console.Clear();
-
-                            Console.WriteLine(" {0} | {1} | {2} ", tabuleiro[0, 0], tabuleiro[0, 1], tabuleiro[0, 2]);
-                            Console.WriteLine("---+---+---");
-                            Console.WriteLine(" {0} | {1} | {2} ", tabuleiro[1, 0], tabuleiro[1, 1], tabuleiro[1, 2]);
-                            Console.WriteLine("---+---+---");
-                            Console.WriteLine(" {0} | {1} | {2} ", tabuleiro[2, 0], tabuleiro[2, 1], tabuleiro[2, 2]);
-                        }
-                        else
-                        {
-                            Console.WriteLine("Posição já ocupada! Tente novamente.");
-
-                        }
+                        
 
                         if ((tabuleiro[0, 0] == "X" && tabuleiro[0, 1] == "X" && tabuleiro[0, 2] == "X") ||
                             (tabuleiro[1, 0] == "X" && tabuleiro[1, 1] == "X" && tabuleiro[1, 2] == "X") ||
@@ -125,16 +68,16 @@ public class Program
                             placarO++;
                             break;
                         }
-                        else if (qtddJogadas == 10)
+                        else if (qttJogadas == 10)
                         {
                             houveGanhador = false;
                             Console.WriteLine("Deu velha!");
                         }
-                    }
+                    
 
-                } while (qtddJogadas <= 9);
+                } while (qttJogadas <= 9);
 
-                qtddJogadas = 1;
+                qttJogadas = 1;
 
                 if (houveGanhador == true)
                 {
@@ -147,24 +90,27 @@ public class Program
             } while (houveGanhador == false || jogarNovamente == 1);
 
         }
+
         else
         {
             Console.WriteLine("Jogo para 1 jogador selecionado.");
             Console.WriteLine("Voce jogará com a máquina.");
 
-            LimparTabuleiro(tabuleiro);
-
-            MostrarTabuleiroVazio();
-
+            Console.WriteLine("Escolha seu símbolo: 'X' ou 'O'");
+            jogadorDaVez = char.Parse(Console.ReadLine());
             DefinirSimbolo(jogadorDaVez);
 
+            LimparTabuleiro(tabuleiro);
+            do
+            {
+                
 
+                EscolherPosicao(ref jogadorDaVez, ref deuCerto, ref posicaoLinha, ref posicaoColuna, ref tabuleiro, ref qttJogadas);
+
+
+            } while (qttJogadas <= 9);
 
         }
-
-         
-
-
     }
 
     private static void MostrarTabuleiroVazio()
@@ -200,13 +146,83 @@ public class Program
             if (jogadorDaVez != 'X' && jogadorDaVez != 'O')
             {
                 Console.WriteLine("Jogador inválido! Tente novamente.");
+                jogadorDaVez = char.Parse(Console.ReadLine());
             }
         } while (jogadorDaVez != 'X' && jogadorDaVez != 'O');
+    }
+
+    private static void EscolherPosicao(ref char jogadorDaVez, ref bool DeuCerto, ref int posicaoLinha, ref int posicaoColuna, ref string[,] tabuleiro, ref int qttJogadas)
+    {
+        do
+        {
+            Console.WriteLine("Jogador {0}, escolha a linha: ", jogadorDaVez);
+            DeuCerto = int.TryParse(Console.ReadLine(), out posicaoLinha);
+            if (DeuCerto == true)
+            {
+                posicaoLinha--;
+            }
+            else
+            {
+                Console.WriteLine("Escreva um número! Tente novamente.");
+            }
+        } while (DeuCerto == false);
+        do
+        {
+            Console.WriteLine("Jogador, escolha a coluna: ");
+            DeuCerto = int.TryParse(Console.ReadLine(), out posicaoColuna);
+            if (DeuCerto == true)
+            {
+                posicaoColuna--;
+            }
+            else
+            {
+                Console.WriteLine("Escreva um número! Tente novamente.");
+            }
+        } while (DeuCerto == false);
+        if (posicaoLinha >= 3 || posicaoColuna >= 3 || posicaoLinha < 0 || posicaoColuna < 0)
+        {
+            Console.WriteLine("Posição inválida! Tente novamente.");
+        }
+        else
+        {
+            if (tabuleiro[posicaoLinha, posicaoColuna] == " ")
+            {
+                tabuleiro[posicaoLinha, posicaoColuna] = jogadorDaVez.ToString();
+                if (jogadorDaVez == 'X')
+                {
+                    jogadorDaVez = 'O';
+                }
+                else
+                {
+                    jogadorDaVez = 'X';
+                }
+                qttJogadas++;
+                Console.Clear();
+                Console.WriteLine(" {0} | {1} | {2} ", tabuleiro[0, 0], tabuleiro[0, 1], tabuleiro[0, 2]);
+                Console.WriteLine("---+---+---");
+                Console.WriteLine(" {0} | {1} | {2} ", tabuleiro[1, 0], tabuleiro[1, 1], tabuleiro[1, 2]);
+                Console.WriteLine("---+---+---");
+                Console.WriteLine(" {0} | {1} | {2} ", tabuleiro[2, 0], tabuleiro[2, 1], tabuleiro[2, 2]);
+            }
+            else
+            {
+                Console.WriteLine("Posição já ocupada! Tente novamente.");
+            }
+
+
+        }
     }
 }
 
 
-/*
+    
+
+
+
+
+
+/* visto dia 02/09
+ 
           *Visibilidade:
           * publica - public
           * privada - private (so a classe consegue acessar)
